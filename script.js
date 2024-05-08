6217f9a1e90dd302a28352cf534a02cdf39db081
MarkEdwardsPractice <markedwardspractice@gmail.com>
2024-05-08 11:55:22 +0900

    Make a Button that prompts the user to enter a number

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const button = document.querySelector(button);
+const button = document.querySelector('button');
 
 button.addEventListener('click', ()=>{
     let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+    
 });
+
