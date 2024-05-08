8ffebb1a2294efb9985d08168ba51a2ce2574284
MarkEdwardsPractice <markedwardspractice@gmail.com>
2024-05-08 13:03:40 +0900

    Make logic to make the game work

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const button = document.querySelector('button');
 
 button.addEventListener('click', ()=>{
     let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-    
+    for(let i = 1; i <= answer; i++)
+        {
+            if(i % 3 === 0 && i % 5 === 0){
+                createPara("FizzBuzz");
+            }
+            else if(i % 3 === 0)
+            {
+                createPara("Fizz");
+            }
+            else if(i % 5 === 0)
+            {
+                createPara("Buzz");
+            }
+            else
+            {
+                createPara(i);
+            }
+        }
 });
 
+function createPara(input)
+{
+    const para = document.createElement('p');
+    para.innerHTML = `${input}`;
+    document.body.appendChild(para);
+}
